1b7fe317632c50d3fd42b26e7da5aea448d1d74e
Weiberle17 <maxweiberle63@gmail.com>
2022-12-03 10:29:45 +0100

    switched to different filemanager

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -52,18 +52,18 @@ end
 beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/nord/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "kitty"
-editor = os.getenv("vim") or "nano"
-editor_cmd = terminal .. " -e " .. editor
-browser = "firefox-esr"
-fileexp = "dolphin"
+local terminal = "kitty"
+local editor = os.getenv("vim") or "nano"
+local editor_cmd = terminal .. " -e " .. editor
+local browser = "firefox-esr"
+local fileexp = "pcmanfm"
 
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
+local modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
